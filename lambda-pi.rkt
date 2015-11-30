4b1aad066f35630f693870195eddb24ff4eeec98
Mark Farrell <m4farrel@uwaterloo.ca>
2015-11-30 07:55:26 -0500

    Add Lambda Pi example

--- lambda-pi.rkt
+++ lambda-pi.rkt
@@ -6,6 +6,10 @@
 (define-type Index Nonnegative-Integer)
 
 (define-datatype Term
+  [If-Then-Else (Term Term Term)]
+  [True-Term ()]
+  [False-Term ()]
+  [Bool ()]
   [Var (Name)]
   [Lam (Name Term)]
   [App (Term Term)]
@@ -18,6 +22,16 @@
 (: substitute (-> Term Name Term Term))
 (define (substitute s x t)
   (match t
+    [(If-Then-Else t1 t2 t3)
+     (If-Then-Else (substitute s x t1)
+                   (substitute s x t2)
+                   (substitute s x t3))]
+    [(True-Term)
+     (True-Term)]
+    [(False-Term)
+     (False-Term)]
+    [(Bool)
+     (Bool)]
     [(Var y)
      (cond [(eq? x y) s]
            [else (Var y)])]
@@ -45,6 +59,10 @@
 (: type-infer (-> Typing-Context Term Term))
 (define (type-infer context term)
   (match term
+    [(True-Term) 
+     (Bool)]
+    [(False-Term)
+     (Bool)]
     [(Var x)
      (hash-ref context x)]
     [(App t1 t2)
@@ -71,6 +89,15 @@
 (: type-check (-> Typing-Context Term Term Term))
 (define (type-check context term ty)
   (match term
+    [(If-Then-Else t1 t2 t3)
+     (let ([ty1 (type-check context t1 (Bool))]
+           [ty2 (type-check context t2 ty)]
+           [ty3 (type-check context t3 ty)])
+       (cond [(and (equal? ty1 (Bool))
+                   (equal? ty2 ty)
+                   (equal? ty3 ty))
+              ty]
+             [else (error "Type mismatch.")]))]
     [(Lam x t1)
      (match ty
        [(Pi x ty1 ty2)
@@ -78,3 +105,15 @@
             (type-check (hash-set context x ty1) t1 ty2))])]
     [_ (cond [(equal? (type-infer context term) ty) ty]
              [else (error "No rule applies.")])]))
+
+(type-check (make-immutable-hash)
+            (Ann (Lam 'x 
+                      (If-Then-Else (Var 'x)
+                                    (False-Term)
+                                    (True-Term))) 
+                 (Pi 'x
+                     (Bool)
+                     (Bool)))
+            (Pi 'x
+                (Bool)
+                (Bool)))
